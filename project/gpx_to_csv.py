07092a9ba5e42048dbc77d2d24eefed3c5e94190
Nima Karimi <nimakarimi676@gmail.com>
2023-08-10 20:11:27 +0200

    added the whole logic to gpx to csv file

--- project/gpx_to_csv.py
+++ project/gpx_to_csv.py
@@ -8,6 +8,29 @@ import gpxpy
 import pandas as pd
 
 
+def haversine_distance(lat1, lon1, lat2, lon2) -> float:
+    distance = hs.haversine(
+        point1=(lat1, lon1),
+        point2=(lat2, lon2),
+        unit=hs.Unit.METERS
+    )
+    return np.round(distance, 2)
+
+
+def add_lift_counter(df):
+    counter = 0
+    df['counter'] = 0  # ? set the counter column to zero
+    for i in range(len(df)):
+        # if df['elevation_diff'][i] < 1:
+        #     continue
+        if df['elevation_diff'][i] > 100:
+            counter += 1
+            # print(i)
+        df['counter'][i] = counter
+
+    # print(df['elevation'][i])
+
+
 def gpx_to_csv(gpx_file_path, csv_file_path):
     with open(gpx_file_path, 'r') as gpx_file:
         gpx = gpxpy.parse(gpx_file)
@@ -24,7 +47,36 @@ def gpx_to_csv(gpx_file_path, csv_file_path):
                 })
 
     route_df = pd.DataFrame(route_info)
+
+    route_df['elevation_diff'] = route_df['elevation'].diff()
+
+    distances = [np.nan]
+    speed = [np.nan]
+
+    for i in range(1, len(route_df)):
+        distances.append(haversine_distance(
+            lat1=route_df.iloc[i - 1]['latitude'],
+            lon1=route_df.iloc[i - 1]['longitude'],
+            lat2=route_df.iloc[i]['latitude'],
+            lon2=route_df.iloc[i]['longitude']
+        ))
+
+        # #* speed
+        time_diff = (route_df.iloc[i].time - route_df.iloc[i - 1].time).seconds
+        speed.append(distances[i]/time_diff)
+
+    route_df['distance'] = distances
+    route_df['speed'] = speed
+
+    route_df['cum_elevation'] = route_df['elevation_diff'].cumsum()
+    route_df['cum_distance'] = route_df['distance'].cumsum()/1e3
+
+    add_lift_counter(route_df)
+
+    route_df = route_df.fillna(0)  # replace NANs with zero
+    ######
     route_df.to_csv(csv_file_path, index=False)
+    return route_df
 
 
 def convert_all_gpx_to_csv(gpx_dir, csv_dir):
@@ -39,4 +91,4 @@ def convert_all_gpx_to_csv(gpx_dir, csv_dir):
 # Usage
 gpx_dir = './data/gpx/'
 csv_dir = './data/csv/'
-# convert_all_gpx_to_csv(gpx_dir, csv_dir)
+convert_all_gpx_to_csv(gpx_dir, csv_dir)
